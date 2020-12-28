8dc079f9a30ee70067eeec703f10da4a3bf5cf1d
Nakhim <chea.nakhim.it@gmail.com>
2020-12-28 23:24:25 +0700

    1. init app 2. Create model 3. Create repository 4. Create Bloc Repository 5. build Home Page

--- lib/model/movie_repository.dart
+++ lib/model/movie_repository.dart
@@ -11,7 +11,6 @@ class MovieResponse {
   MovieResponse.fromJson(List json)
       : movies = (json).map((i) => new Movie.fromJson(i)).toList(),
         error = "";
-
   MovieResponse.withError(String errorValue)
       : movies = List(),
         error = errorValue;
