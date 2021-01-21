f57119a7f6de8ad13d8618b5dc0e4ceca732c93b
Nakhim <chea.nakhim.it@gmail.com>
2021-01-22 00:29:26 +0700

    Item Detal

--- lib/model/video_play_screen.dart
+++ lib/model/video_play_screen.dart
@@ -1,4 +1,5 @@
 import 'package:better_player/better_player.dart';
+import 'package:fijkplayer/fijkplayer.dart';
 import 'package:flutter/material.dart';
 
 
@@ -13,11 +14,13 @@ class VideoPlayScreen extends StatefulWidget {
 
 class _VideoPlayScreenState extends State<VideoPlayScreen> {
   BetterPlayerController _betterPlayerController;
+  final FijkPlayer player = FijkPlayer();
 
 
   @override
   void initState() {
     super.initState();
+    player.setDataSource(widget.url, autoPlay: true, showCover: true);
     BetterPlayerDataSource betterPlayerDataSource = BetterPlayerDataSource(
       BetterPlayerDataSourceType.network,
       "${widget.url}",
@@ -30,6 +33,7 @@ class _VideoPlayScreenState extends State<VideoPlayScreen> {
         "Accept-Language" : "en-US,en;q=0.5",
         "Connection" : "keep-alive",
         "Host" : "www03.cloud9xx.com",
+        "Origin" : "https://players.akamai.com",
         "Referer" : "https://gogo-play.net",
         "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0",
       }
@@ -59,6 +63,20 @@ class _VideoPlayScreenState extends State<VideoPlayScreen> {
   }
   @override
   Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: FijkView(
+        player: player,
+        width: double.infinity,
+        height: double.infinity,
+        fit: FijkFit.cover,
+        fsFit : FijkFit.cover,
+        color: Colors.black,
+        onDispose: (value){
+          player.dispose();
+        },
+      ),
+    );
     return AspectRatio(
       aspectRatio: 16 / 9,
       child: BetterPlayer(
